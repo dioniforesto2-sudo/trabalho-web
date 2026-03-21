6e0d6b7b6c52ac850bc17b9310f216e1d1c01cb4
dioniforesto2-sudo <dioniforesto2@gmail.com>
2026-03-20 22:24:16 -0300

    Add files via upload

---
+++ usuario.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<main>
+  <div class="header-content">
+    <h2>Gestão de Usuários</h2>
+    <p>Visualize e gerencie as permissões dos usuários do sistema.</p>
+  </div>
+
+  <div class="table-responsive">
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>E-mail</th>
+          <th>Acesso</th>
+          <th>Status</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>01</td>
+          <td>Dioni Foresto</td>
+          <td>dioniforesto03@gmail.com</td>
+          <td>Administrador</td>
+          <td><span class="badge ativo">Ativo</span></td>
+          <td><button class="btn-icon"><i class="fa-solid fa-pen"></i></button></td>
+        </tr>
+        <tr>
+          <td>02</td>
+          <td>Nycolas</td>
+          <td>nycolas@email.com</td>
+          <td>Editor</td>
+          <td>Ativo</td>
+          <td>Editar</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</main>
+</html>
