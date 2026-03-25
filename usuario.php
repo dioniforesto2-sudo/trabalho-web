d1a2631ccd95b1ba4cf68cb054feb3d368601a6c
dioniforesto2-sudo <dioniforesto2@gmail.com>
2026-03-25 16:36:48 -0300

    Add files via upload

--- usuario.php
+++ usuario.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html>
-<main>
-  <div class="header-content">
+    <head>
+        <title> Sistema</title>
+        <link rel="stylesheet" href="dash.css">
+          <meta charset="UTF-8">
+    </head>
+<body>
+    <div class="header-content">
     <h2>Gestão de Usuários</h2>
     <p>Visualize e gerencie as permissões dos usuários do sistema.</p>
   </div>
@@ -39,4 +44,53 @@
     </table>
   </div>
 </main>
-</html>
+        
+    <div class="dashboard-container">
+        <nav>
+            <ul>
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Receitas</a></li>
+                <li><a href="#"> Imagens</a></li>
+                <li><a href="#">Configurações</a></li>
+            </ul>
+            <div class="perfil-usuario">
+            <img src="https://img.freepik.com/vetores-gratis/jogo-de-astronauta-fofo-com-joystick-e-fone-de-ouvido-desenho-animado-vetor-icone-ilustracao-ciencia-techno_138676-9648.jpg?semt=ais_hybrid&w=740&q=80">
+            <span>Dioni Foresto</span>
+        </nav>
+        </nav>
+
+        <main> 
+            <section class="container-cards">
+
+                <article>
+                    <h2>Receitas</h2>
+                    <p>Veja receitas elaboradas por nós</p>
+                    <a href="#" class="btn">Acessar</a>
+
+                </article>
+                <section class="container cards">
+                    <article>
+                        <h2>Cozinheiros</h2>
+                        <p>Conheça aqui nossos cozinheiros</p>
+                        <a href="#" class="btn">Acessar</a>
+                    </article>
+                    <section class="container cards">
+                        <article>
+                            <h2>Apps auxiliares</h2>
+                            <p>Aqui está alguns aplicativos que podem ser úteis</p>
+                            <a href="#" class="btn">Acesse aqui</a>
+                        </article>
+    </body>
+                        <footer>
+                            <p>&copy; <?php echo date('Y'); ?> - Desenvolvido na aula web 1</p>
+
+                        </footer>
+                        
+
+                    
+
+                
+
+            
+        </main>
+    </div>
